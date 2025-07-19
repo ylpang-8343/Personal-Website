0eace9a0d0c06706efe17440c79d7eaa80dc3065
Pang Yoke Leap <pangyokeleap@gmail.com>
2025-07-19 20:35:25 +0800

    Update

--- js/bullshit.js
+++ js/bullshit.js
@@ -1,16 +1,15 @@
 const texts = [
     "Cabbage is not delicious.",
-    "WTF why there are so many bugs?",
-    "Bugs? Will be fixed at 32 December.",
+    "Try Minecraft, it's fun.",
+    "This is not a bug, it's a feature. --Minecraft community",
     "Money? Do I look like a bank?",
     "Finding a bug for 99 years and found that a tag is lost.",
-    "Still refreshing? Go find a girlfriend.",
-    "Stop refreshing, I am not a bot.",
-    "Still refreshing? RM100 for 1 refresh.",
-    "Boring? Go find a girlfriend.",
-    "Empty wallet after a month of work.",
-    "!@#$%^&*()_+",
-    "诶嘿，以为没有中文？",
+    "I thought that you wont see this.",
+    "No refreshing, nothing here.",
+    "Being fired because of bringing durian to work.",
+    "Myvi semua pun boleh.",
+    "Don't ask me to play MOBA games, f*ck that.",
+    "ZZZZZ... Oh, still here?",
 ];
 
 const index = Math.floor(Math.random() * texts.length);
